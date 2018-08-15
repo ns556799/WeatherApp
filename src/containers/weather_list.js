ff276af491b896941ae9832000ae7f51d2368a7f
Niv Suresh <nivsuresh06@gmail.com>
2018-08-15 20:25:00 +0100

    Added Sparklines to create linechart for each city

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,26 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const {name} = cityData[0].city;
+        const temps = cityData[0].list.map((weather) => weather.main.temp);
+
+        console.log(temps);
         return (
             <tr key={name}>
                 <td>
                     {name}
+                </td>
+                <td>
+                    <Sparklines
+                        height={120}
+                        width={180}
+                        data={temps}
+                    >
+                        <SparklinesLine color="blue" />
+                    </Sparklines>
                 </td>
             </tr>
         );
